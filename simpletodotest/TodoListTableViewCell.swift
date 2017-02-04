09dd2a7ddac8d5b34218eb350f4faf52c35c6667
Dmytrii <t4ec@users.noreply.github.com>
2017-02-04 14:39:11 +0200

    Add data source methods for todo lists

--- simpletodotest/TodoListTableViewCell.swift
+++ simpletodotest/TodoListTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TodoListTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var title: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
