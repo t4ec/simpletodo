1603164955f2b61281f99e88f523913165754878
Dmytrii <t4ec@users.noreply.github.com>
2017-02-04 12:47:34 +0200

    Integrate facebook login button

--- simpletodotest/AppDelegate.swift
+++ simpletodotest/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,13 +17,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        
+        let success = FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let _ = AccessToken.current {
+            let mainViewController = storyboard.instantiateViewController(withIdentifier: "Main")
+            window?.rootViewController = mainViewController
+            window?.makeKeyAndVisible()
+        } else {
+            let loginViewController = storyboard.instantiateViewController(withIdentifier: "Login")
+            window?.rootViewController = loginViewController
+            window?.makeKeyAndVisible()
+        }
+
+        return success
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
---
+++ simpletodotest/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  simpletodotest
+//
+//  Created by Dmytrii Sinko on 04.02.17.
+//  Copyright © 2017 Dmytrii Sinko. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+import FacebookCore
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let accessToken = AccessToken.current {
+            // nothing
+        } else {
+            let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+            loginButton.center = view.center
+            view.addSubview(loginButton)
+        }
+    }
+}
+
